44a87c4594158b0516c695e885fc66cdd6303c69
Dylan Spyer <dylanspyer@gmail.com>
2022-11-16 09:30:13 -0600

    created simple UI to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,16 +1,10 @@
-// Computer randomly selects one of the three choices
-const gameChoices = ["rock", "paper", "scissors"];
-
-function getComputerChoice() {
-  const randomIndex = Math.floor(Math.random() * gameChoices.length);
-  const randomChoice = gameChoices[randomIndex];
-  return randomChoice;
-}
-
 const playerText = document.querySelector(".playerText");
 const computerText = document.querySelector(".computerText");
 const resultText = document.querySelector(".resultText");
 const choiceButton = document.querySelectorAll(".choiceButton");
+const playerScorePara = document.querySelector(".playerScorePara");
+const computerScorePara = document.querySelector(".computerScorePara");
+const gameOver = document.querySelector(".gameOver");
 
 choiceButton.forEach((button) =>
   button.addEventListener("click", () => {
@@ -22,42 +16,58 @@ choiceButton.forEach((button) =>
       playerSelection,
       computerSelection
     )}`;
+    computerScorePara.textContent = `Computer Score: ${computerScore}`;
+    playerScorePara.textContent = `Player Score: ${playerScore}`;
+    if (playerScore === 5) {
+      gameOver.textContent = "Game over, you win!";
+      resetGame();
+    }
+    if (computerScore === 5) {
+      gameOver.textContent = "Game over, you lose!";
+      resetGame();
+    }
   })
 );
+
+function resetGame() {
+  computerScore = 0;
+  playerScore = 0;
+}
 
 let computerScore = 0;
 let playerScore = 0;
 
+const gameChoices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * gameChoices.length);
+  const randomChoice = gameChoices[randomIndex];
+  return randomChoice;
+}
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose";
     computerScore++;
+    return "You lose";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You win";
     playerScore++;
+    return "You win";
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lose";
     computerScore++;
+    return "You lose";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You win";
     playerScore++;
+    return "You win";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lose";
     computerScore++;
+    return "You lose";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You win";
     playerScore++;
+    return "You win";
   } else {
     return "It's a tie";
   }
 }
-
-// console.log(playerSelection);
-// console.log(computerSelection);
-
-// console.log(playRound(playerSelection, computerSelection));
-
-// log result via a text prompt stating either "you win", "you lose", or "tie"
 
 // When playRound is run
 // Create a running tally of how many times play won and how many times computer won
